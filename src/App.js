51708f1932c94b0f68b0724a4d7aa24a376ec5df
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2022-03-02 16:07:11 -0600

    up

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import Counter from "./Counter";
 
 function App() {
   const [hide, setHide] = useState(false);
-  const [idInterval, setIdInterval] = useState(null)
-
-  useEffect(() => {
-    window.clearInterval(idInterval)
-  },[hide])
 
   return (
     <div className="App">
       <h1 onClick={()=> setHide(!hide)}>{hide?"Hide":"Show"}</h1>
-      {hide&& <Counter setIdInterval={setIdInterval} />}
+      {hide&& <Counter />}
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,19 @@
 import {useEffect, useState} from "react";
 
-const Counter = ({setIdInterval}) => {
+const Counter = () => {
   const [counter, setCounter] = useState(0)
+  const [idInterval, setIdInterval] = useState(0)
+
 
   useEffect( async ()=> {
     setIdInterval(setInterval(function() {
       setCounter((counter) => counter + 1);
     }, 1000))
+
+    return () => {
+      window.clearInterval(idInterval)
+    }
+
   },[])
   return (
     <div>
